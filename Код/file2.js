e812629363d05b63db6da20f4d9184f64e0928da
Ryabova2020 <76618274+EkaEkaEkaRy@users.noreply.github.com>
2023-10-21 19:15:38 +0300

    change main

--- Код/file2.js
+++ Код/file2.js
@@ -4,5 +4,18 @@ function tomain() {
     if (str1 !== str2) {
      alert("Пароли не совпадают")
     }
+    else if (isEmpty(str1)) alert("Поле пустое");
     else return location.href='main.html'
+}
+
+function isEmail() {
+    var str = document.getElementById("login").value;
+    var re = /^[^\s()<>@,;:\/]+@\w[\w\.-]+\.[a-z]{2,}$/i;
+    if(isEmpty(str)) alert("Поле пустое");
+    else if (!re.test(str)) alert("Адрес неверный");
+    else tomain()
+}
+   
+function isEmpty(str){
+    return (str == null) || (str.length == 0);
 }
