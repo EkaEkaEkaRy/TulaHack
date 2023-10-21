8f6968e811284a6e4d6dd28b76a843163764549f
Ryabova2020 <76618274+EkaEkaEkaRy@users.noreply.github.com>
2023-10-21 19:43:32 +0300

    change file1

---
+++ Код/file1.js
@@ -0,0 +1,17 @@
+function tomain() {
+    var str1 = document.getElementById("password").value;
+    if (isEmpty(str1)) alert("Поле пустое");
+    else return location.href='main.html'
+}
+
+function isEmail() {
+    var str = document.getElementById("mail").value;
+    var re = /^[^\s()<>@,;:\/]+@\w[\w\.-]+\.[a-z]{2,}$/i;
+    if(isEmpty(str)) alert("Поле пустое");
+    else if (!re.test(str)) alert("Адрес неверный");
+    else tomain()
+}
+   
+function isEmpty(str){
+    return (str == null) || (str.length == 0);
+}
